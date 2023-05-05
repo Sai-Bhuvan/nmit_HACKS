4e56c6e16e5ee7cfc73a529e000e6ae4d4521c57
Harshith C S <107236868+harshith082003@users.noreply.github.com>
2023-05-05 18:48:58 +0530

    setPin page added

--- ClientSideApp/App.js
+++ ClientSideApp/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignUP from './screens/SignUP';
 
 export default function App() {
   return (
     <View style={styles.container}>
+<<<<<<< Updated upstream
       <Text>Hi</Text>
       <StatusBar style="auto" />
+=======
+      <SignUP/>      
+>>>>>>> Stashed changes
     </View>
   );
 }
--- MerchantSideApp/App.js
+++ MerchantSideApp/App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+<<<<<<< Updated upstream
 import SignIn from './screens/signin';
+=======
+import SetPin from './screens/SetPin';
+>>>>>>> Stashed changes
 
 export default function App() {
   return (
     <View style={styles.container}>
+<<<<<<< Updated upstream
       <SignIn mobno={"738753837"}/>
       <StatusBar style="auto" />
+=======
+      <SetPin/>
+>>>>>>> Stashed changes
     </View>
   );
 }
