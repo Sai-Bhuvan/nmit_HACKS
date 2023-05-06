6dc61cb2226da59e8590c05e0c760d5fc54db9dd
MVNihalNarayanRao <MVNihalNarayanRao@users.noreply.github.com>
2023-05-06 09:49:41 +0530

    previous transaction

--- backend/routes/transaction/previousTransactions.js
+++ backend/routes/transaction/previousTransactions.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { getDb } = require('../../index');
 
-router.post('/previousTranactions/', async function(req, res) {
-    const to = req.body.to;
+router.post('/previousTranactions/', async function (req, res) {
+    const phone = req.body.phone;
     try {
         const db = getDb();
         const mycoll = db.collection("transactions");
-        const result = mycoll.find({'to': to}).toArray();
+        const result = await mycoll.find({ $or: [{ 'to': phone }, { 'from': phone }] }).toArray();
         res.status(200).json({ transactions: result, statusCode: 200, message: "success" });
     }
-    catch(e) {
+    catch (e) {
         res.status(400).json({ message: e.message, statusCode: 400 });
     }
 });
