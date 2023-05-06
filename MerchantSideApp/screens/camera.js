c4a1a42eb3bcdd658d931f29de63ce97d81fdfda
Sai-Bhuvan <anna.bhuvan@gmail.com>
2023-05-06 12:16:06 +0530

    camera

--- MerchantSideApp/screens/camera.js
+++ MerchantSideApp/screens/camera.js
@@ -1,6 +1,7 @@
-import { View, Text ,StyleSheet,Image} from 'react-native'
+import { View, Text ,StyleSheet,Image, Alert} from 'react-native'
 import React, { useState,useEffect, useRef } from 'react'
-import { Camera,CameraType } from 'expo-camera';
+import { Camera,CameraType} from 'expo-camera';
+import * as FaceDetector from 'expo-face-detector';
 import * as MediaLibrary from 'expo-media-library';
 import Button from './src/Components/Button';
 
@@ -10,6 +11,7 @@ export default function camera() {
     const [image,setimage]=useState(null);
     const [type,settype]=useState(Camera.Constants.Type.back);
     const [flash,setflash]=useState(Camera.Constants.FlashMode.off);
+    const [detect,setdetect]=useState(false);
     const cameraref=useRef(null);
 
     useEffect(()=>{
@@ -19,6 +21,28 @@ export default function camera() {
             sethasCameraPermission(camerastatus.status==="granted");
         })();
     },[])
+
+    
+
+    const handleFacesDetected = ({ faces }) => { 
+        if(faces.length==1){
+            setdetect(true);
+        }else{
+            setdetect(false);
+        }
+     };
+
+     const saveimage=async () => {
+        if(image){
+            try{
+                await MediaLibrary.createAssetAsync(image);
+                Alert.alert('picture saved')
+                setimage(null);
+            }catch(e){
+                console.log(e);
+            }
+        }
+     }
 
     const takepicture=async () =>{
         if(cameraref){
@@ -40,19 +64,50 @@ export default function camera() {
     <View style={styles.Container}>
         {!image?
       <Camera
+        onFacesDetected={handleFacesDetected}
+        faceDetectorSettings={{
+            mode: FaceDetector.FaceDetectorMode.fast,
+            detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
+            runClassifications: FaceDetector.FaceDetectorClassifications.none,
+            minDetectionInterval: 100,
+            tracking: true,
+        }}
         style={StyleSheet.Camera}
         type={type}
         flashMode={flash}
         ref={cameraref}
       >
-        <Text>Hello</Text>
+        <View style={styles.cameraview}>
+            <Button icon={'retweet'} onpress={()=>{
+                settype(type===CameraType.back?CameraType.front:CameraType.back)
+            }}/>
+            <Button 
+            icon={'flash'} 
+            color={flash===Camera.Constants.FlashMode.off?'gray':'#f1f1f1'}
+            onpress={
+                ()=>{
+                    setflash(flash===Camera.Constants.FlashMode.off?
+                        Camera.Constants.FlashMode.on:
+                        Camera.Constants.FlashMode.off)
+                }
+            }
+            />
+        </View>
       </Camera>
       :
-      <Image />
+      <Image source={{uri:image}} style={styles.camera}/>
         }
       <View>
-        <Button title={'take a p'} onpress={takepicture}/>
+        {image?
+        <View style={styles.camerabut}>
+            <Button title={"retake" } icon={"retweet"} onpress={()=>setimage(null)}/>
+            <Button title={"save" } icon={"check"} onpress={saveimage}/>
+        </View>
+        :
+        <Button title={'take a p'} onpress={takepicture} disabled={takepicture} />
+        }
       </View>
+
     </View>
   )
 }
@@ -67,5 +122,15 @@ const styles=StyleSheet.create({
         backgroundColor:'#000',
         justifyContent:'center',
         padding:15
+    },
+    camerabut:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingHorizontal:15,
+    },
+    cameraview:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        padding:30
     }
 })
